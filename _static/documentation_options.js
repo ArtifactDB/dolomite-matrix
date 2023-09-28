f96110c5a1b42c8c68bc5183d54eb339f98683f4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-28 22:47:14 +0000

    Deploying to gh-pages from @ ArtifactDB/dolomite-matrix@11e7ba6d626191a0895bd9c5311969490fc5fa74 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gf73a482',
+    VERSION: '0.0.post1.dev1+g11e7ba6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
